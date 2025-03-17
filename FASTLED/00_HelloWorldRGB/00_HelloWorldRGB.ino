db4a1f4999d474b851bbbb7313550b70fab9e7cd
jlsgangwisch <jlsgangwisch@gmail.com>
2025-03-17 12:01:35 -0400

    update

--- FASTLED/00_HelloWorldRGB/00_HelloWorldRGB.ino
+++ FASTLED/00_HelloWorldRGB/00_HelloWorldRGB.ino
@@ -1,14 +1,18 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 1000
+#define NUM_LEDS 10
 #define LED_PIN 2
 
 int idx = 0;
 
 CRGB leds[NUM_LEDS];
+CRGB leds1[NUM_LEDS];
+
 
 void setup() {
+  Serial.begin(9600);
   FastLED.addLeds<WS2815, LED_PIN, RGB>(leds, NUM_LEDS);
+   FastLED.addLeds<WS2815, 15, RGB>(leds1, NUM_LEDS);
   FastLED.setBrightness(20);
 
 }
@@ -16,11 +20,16 @@ void setup() {
 void loop() {
   leds[idx-1] = CRGB::Black;
   leds[idx] = CRGB::Blue;
+  leds1[idx-1] = CRGB::Black;
+  leds1[idx] = CRGB::Green;
   FastLED.show();
-  delay(2);
+  delay(200);
   idx += 1;
 
-  if (idx> NUM_LEDS){
+  if (idx>= NUM_LEDS){
+    leds[idx-1] = CRGB::Black;
+    leds1[idx-1] = CRGB::Black;
     idx = 0;
   }
+  Serial.println(idx);
 }
--- FASTLED/00_SeedLight_Blink_Each/00_SeedLight_Blink_Each.ino
+++ FASTLED/00_SeedLight_Blink_Each/00_SeedLight_Blink_Each.ino
@@ -1,19 +1,30 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 1000
+#define NUM_LEDS 10
 #define LED_PIN 2
 
 CRGB leds[NUM_LEDS];
 
+bool on = false;
+
 void setup() {
+  Serial.begin(9600);
   FastLED.addLeds<WS2815, LED_PIN, RGB>(leds, NUM_LEDS);
   FastLED.setBrightness(50);
 
 }
 
 void loop() {
-  leds[0] = CRGB::Red;
-  leds[1] = CRGB::Green;
-  leds[2] = CRGB::Blue;
+  for (int i; i<NUM_LEDS; i++){
+    if (on){
+       leds[i] = CRGB::White;
+    } else {
+       leds[i] = CRGB::Black;
+    }
+  }
+
   FastLED.show();
+  delay(1000);
+  on = !on;
+  Serial.println(on);
 }
